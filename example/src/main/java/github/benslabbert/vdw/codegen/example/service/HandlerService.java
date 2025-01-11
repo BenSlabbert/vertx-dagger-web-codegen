8ffbfb0b79cc0f62980bceae8f6bb493f12f6a39
benjamin.slabbert <benjamin.slabbert@gmail.com>
2025-01-11 18:35:41 +0100

    update build

--- example/src/main/java/github/benslabbert/vdw/codegen/example/service/HandlerService.java
+++ example/src/main/java/github/benslabbert/vdw/codegen/example/service/HandlerService.java
@@ -6,7 +6,6 @@ import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Transactional
 public class HandlerService {
 
   private static final Logger log = LoggerFactory.getLogger(HandlerService.class);
