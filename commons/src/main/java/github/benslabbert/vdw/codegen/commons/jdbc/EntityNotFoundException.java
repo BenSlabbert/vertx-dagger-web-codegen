8711736a5e61d0e1454556d372b5146bbb85e512
benjamin <benjamin.slabbert@gmail.com>
2025-10-22 19:39:53 +0200

    adding exception
    # Conflicts:
    #	pom.xml

---
+++ commons/src/main/java/github/benslabbert/vdw/codegen/commons/jdbc/EntityNotFoundException.java
@@ -0,0 +1,8 @@
+/* Licensed under Apache-2.0 2025. */
+package github.benslabbert.vdw.codegen.commons.jdbc;
+
+public class EntityNotFoundException extends RuntimeException {
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+}
