29f22e106d7f4fae222ed05ee9fc3256c7549c86
benjamin <benjamin.slabbert@gmail.com>
2025-11-28 19:55:12 +0100

    fixing deployment

--- example/src/main/java/github/benslabbert/vdw/codegen/example/Main.java
+++ example/src/main/java/github/benslabbert/vdw/codegen/example/Main.java
@@ -2,10 +2,8 @@
 package github.benslabbert.vdw.codegen.example;
 
 import github.benslabbert.vdw.codegen.example.jdbc.Person;
-import io.vertx.core.ThreadingModel;
-import io.vertx.launcher.application.HookContext;
+import github.benslabbert.vdw.codegen.launcher.CustomApplicationHooks;
 import io.vertx.launcher.application.VertxApplication;
-import io.vertx.launcher.application.VertxApplicationHooks;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,35 +16,14 @@ public class Main extends VertxApplication {
     int code = new Main(args).launch();
     log.info("launch successful ? {}", 0 == code);
 
-    Person person = Person.builder().id(1L).name("name").build();
+    Person person =
+        Person.builder().id(1L).name("name").lastName("lname").age(1).gender("M").build();
     log.info("person {}", person);
-    person = person.toBuilder().build();
-    log.info("person {}", person);
+    Person newPerson = person.toBuilder().build();
+    log.info("newPerson {}", newPerson);
   }
 
   private Main(String[] args) {
-    super(
-        args,
-        new VertxApplicationHooks() {
-          @Override
-          public void beforeDeployingVerticle(HookContext context) {
-            log.info("beforeDeployingVerticle");
-            context
-                .deploymentOptions()
-                .setThreadingModel(ThreadingModel.VIRTUAL_THREAD)
-                .setWorkerPoolSize(1)
-                .setWorkerPoolName("worker-pool");
-          }
-
-          @Override
-          public void beforeStartingVertx(HookContext context) {
-            log.info("beforeStartingVertx");
-            context
-                .vertxOptions()
-                .setWorkerPoolSize(1)
-                .setEventLoopPoolSize(1)
-                .setInternalBlockingPoolSize(1);
-          }
-        });
+    super(args, new CustomApplicationHooks());
   }
 }
--- example/src/main/java/github/benslabbert/vdw/codegen/example/verticle/DefaultVerticle.java
+++ example/src/main/java/github/benslabbert/vdw/codegen/example/verticle/DefaultVerticle.java
@@ -8,9 +8,7 @@ import github.benslabbert.vdw.codegen.example.web.RouterFactory;
 import github.benslabbert.vdw.codegen.example.web.ServerFactory;
 import github.benslabbert.vdw.codegen.txmanager.PlatformTransactionManager;
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Handler;
 import io.vertx.core.Promise;
-import io.vertx.core.eventbus.DeliveryContext;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
@@ -25,6 +23,7 @@ public class DefaultVerticle extends AbstractVerticle {
 
   private Provider provider = null;
   private HttpServer httpServer = null;
+  long periodic = -1L;
 
   private void setHttpServer(HttpServer httpServer) {
     this.httpServer = httpServer;
@@ -37,6 +36,8 @@ public class DefaultVerticle extends AbstractVerticle {
   @Override
   public void start(Promise<Void> startPromise) {
     log.info("Starting verticle");
+    vertx.exceptionHandler(throwable -> log.error("unhandled exception", throwable));
+
     provider = DaggerProvider.builder().vertx(vertx).config(config()).build();
     provider.init();
     provider.eventBusServiceConfigurers().forEach(EventBusServiceConfigurer::configure);
@@ -65,21 +66,28 @@ public class DefaultVerticle extends AbstractVerticle {
   private void eb() {
     // internally, vert.x will always call the handlers in the order they are registered on the
     // eventbus.
-    vertx
-        .eventBus()
-        .addInboundInterceptor(
-            (Handler<DeliveryContext<JsonObject>>) e -> e.message().fail(401, ""));
+    log.info("Starting EB");
     vertx.eventBus().consumer(ADDR, msg -> log.info("c1 received: {}", msg.body()));
     vertx.eventBus().consumer(ADDR, msg -> log.info("c2 received: {}", msg.body()));
 
-    vertx.eventBus().publish(ADDR, new JsonObject().put("key", "val1"));
-    vertx.eventBus().publish(ADDR, new JsonObject().put("key", "val2"));
-    vertx.eventBus().publish(ADDR, new JsonObject().put("key", "val3"));
+    periodic =
+        vertx.setPeriodic(
+            1000L,
+            1000L,
+            ignore -> {
+              log.info("timer");
+              vertx
+                  .eventBus()
+                  .publish(ADDR, new JsonObject().put("key", "val1"))
+                  .publish(ADDR, new JsonObject().put("key", "val2"))
+                  .publish(ADDR, new JsonObject().put("key", "val3"));
+            });
   }
 
   @Override
   public void stop(Promise<Void> stopPromise) {
     log.info("Stopping verticle");
+    vertx.cancelTimer(periodic);
 
     try {
       PlatformTransactionManager.close();
