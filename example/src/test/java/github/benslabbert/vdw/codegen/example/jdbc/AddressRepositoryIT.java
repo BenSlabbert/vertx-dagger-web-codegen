cf6eb47c810f0920a1352524ed70bcf8aceff40f
benjamin <benjamin.slabbert@gmail.com>
2025-12-06 18:28:12 +0100

    fmt

--- example/src/test/java/github/benslabbert/vdw/codegen/example/jdbc/AddressRepositoryIT.java
+++ example/src/test/java/github/benslabbert/vdw/codegen/example/jdbc/AddressRepositoryIT.java
@@ -4,91 +4,16 @@ package github.benslabbert.vdw.codegen.example.jdbc;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 import github.benslabbert.vdw.codegen.commons.jdbc.EntityNotFoundException;
-import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class AddressRepositoryIT extends PostgresTestBase {
-
-  private Provider provider;
-
-  @BeforeEach
-  void init() {
-    HikariConfig hikariConfig = new HikariConfig();
-    hikariConfig.setJdbcUrl(
-        "jdbc:postgresql://localhost:%d/postgres".formatted(POSTGRES.getMappedPort(5432)));
-    hikariConfig.setUsername("postgres");
-    hikariConfig.setPassword("postgres");
-    hikariConfig.setAutoCommit(false);
-    hikariConfig.setMaximumPoolSize(2); // Increased for optimistic locking concurrency tests
-    hikariConfig.setPoolName("jdbc");
-    hikariConfig.setThreadFactory(Thread.ofVirtual().name("v-", 0L).factory());
-
-    provider = DaggerProvider.builder().dataSource(new HikariDataSource(hikariConfig)).build();
-    provider.init();
-
-    try (var c = provider.dataSource().getConnection();
-        var s =
-            c.prepareStatement(
-                """
-                 create sequence id_seq
-                    start 1
-                    increment 1
-                    cache 1;
-
-                 create table address (
-                    id bigint primary key,
-                    version int4 not null,
-                    street text,
-                    postal_code text
-                 );
-
-                 create table person (
-                    id bigint primary key,
-                    version int4,
-                    first_name text,
-                    middle_name text,
-                    last_name text,
-                    age int4,
-                    gender text,
-                    address_id bigint not null unique,
-                    constraint person_address_fk foreign key (address_id) references address(id)
-                 );
-                """)) {
-      s.execute();
-      c.commit();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  @AfterEach
-  void after() {
-    try (var c = provider.dataSource().getConnection();
-        var s = c.createStatement()) {
-      s.execute(
-          """
-           drop table person;
-           drop table address;
-           drop sequence id_seq;
-          """);
-      c.commit();
-    } catch (SQLException e) {
-      throw new RuntimeException(e);
-    } finally {
-      provider.close();
-    }
-  }
 
   @Test
   void save() {
@@ -379,9 +304,7 @@ class AddressRepositoryIT extends PostgresTestBase {
             .jdbcTransactionManager()
             .executeWithResult(
                 () ->
-                    provider
-                        .addressRepository()
-                        .requireIdAndVersion(saved.id(), saved.version()));
+                    provider.addressRepository().requireIdAndVersion(saved.id(), saved.version()));
 
     assertThat(found).isEqualTo(saved);
   }
@@ -456,8 +379,7 @@ class AddressRepositoryIT extends PostgresTestBase {
                         () ->
                             provider
                                 .addressRepository()
-                                .save(
-                                    AddressBuilder.toBuilder(saved).postalCode("pc3").build())))
+                                .save(AddressBuilder.toBuilder(saved).postalCode("pc3").build())))
         .isInstanceOf(RuntimeException.class);
   }
 
